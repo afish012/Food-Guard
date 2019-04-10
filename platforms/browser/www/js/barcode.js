6fe49c913a102f3ca810874bfa095aabcb3b7cf2
Faiz <Faiz@FN-MacBook-Air.local>
2019-04-10 21:06:50 +0100

    scan

--- platforms/browser/www/js/barcode.js
+++ platforms/browser/www/js/barcode.js
@@ -1,53 +1,5 @@
 app.initialize();
 
-function process() {
-  Quagga.decodeSingle({
-    decoder: {
-      readers: ["code_128_reader", "ean_reader"] // List of active readers
-    },
-    locate: true, // try to locate the barcode in the image
-    src: document.getElementById('myImage').src, // or 'data:image/jpg;base64,' + data
-  }, function(result) {
-    if (result.codeResult) {
-      console.log("result", result.codeResult.code);
-      document.getElementById("result").innerHTML = "Barcode data = " + result.codeResult.code;
-      getJSON(5054775980437);
-    } else {
-      console.log("not detected");
-    }
-  });
-}
-
-////// OLD BARCODE SCANNING //////
-function setOptions() {
-  var options = {
-    // Some common settings are 20, 50, and 100
-    quality: 100,
-    destinationType: Camera.DestinationType.FILE_URI,
-    encodingType: Camera.EncodingType.JPEG,
-    mediaType: Camera.MediaType.PICTURE,
-    allowEdit: false,
-    correctOrientation: true, //Corrects Android orientation quirks
-    cameraDirection: 1
-
-  }
-  return options;
-}
-
-////// OLD BARCODE SCANNING //////
-function cameraTakePicture() {
-  navigator.camera.getPicture(onSuccess, onFail, setOptions());
-
-  function onSuccess(imageData) {
-    var image = document.getElementById('myImage');
-    image.src = imageData;
-  }
-
-  function onFail(message) {
-    alert('Failed because: ' + message);
-  }
-}
-
 function getJSON(barcode) {
   //help here (example code) -> https://www.quackit.com/json/tutorial/json_with_http_jquery.cfm
   $.getJSON("https://world.openfoodfacts.org/api/v0/product/" + barcode + ".json", function(result) {
@@ -56,12 +8,18 @@ function getJSON(barcode) {
 
     if (status == "product not found") {
       document.getElementById("product").innerHTML = status;
+      $("#scanpagebody").removeClass("hidden")
+       $("#success").addClass("hidden")
+
+
     } else {
       var product = result.product.product_name;
       var allergens = result.product.allergens_tags;
-      document.getElementById("product").innerHTML = "Product Name = " + product;
+      document.getElementById("product").innerHTML = product;
       document.getElementById("allergens").innerHTML = "Allergens = " + allergens;
-      document.getElementById("result").innerHTML = "Barcode data = " + barcode;
+      // document.getElementById("result").innerHTML = "Barcode data = " + barcode;
+      $("#scanpagebody").addClass("hidden")
+      $("#success").removeClass("hidden")
     }
   });
 
